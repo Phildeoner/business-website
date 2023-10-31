d078ae7ef18196fb09b4998b72d024a00c49af67
Yakubu Bobai Ephraim <phildeoner@gmail.com>
2023-10-31 16:16:43 +0100

    Update Navbar component by adding shadow and background color (#4)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,15 +4,15 @@ import { Button, Navbar } from "flowbite-react";
 function NavBar() {
   return (
     <>
-      <Navbar fluid rounded>
+      <Navbar fluid rounded className="shadow-md bg-gray-100">
         <Navbar.Brand href="https://flowbite-react.com">
           <img
             src="/favicon.svg"
             className="mr-3 h-6 sm:h-9"
-            alt="Flowbite React Logo"
+            alt="Company Logo"
           />
           <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-            Flowbite React
+            Business Website
           </span>
         </Navbar.Brand>
         <div className="flex md:order-2">
@@ -24,8 +24,7 @@ function NavBar() {
             Home
           </Navbar.Link>
           <Navbar.Link href="#">About</Navbar.Link>
-          <Navbar.Link href="#">Services</Navbar.Link>
-          <Navbar.Link href="#">Pricing</Navbar.Link>
+          <Navbar.Link href="#">Products</Navbar.Link>
           <Navbar.Link href="#">Contact</Navbar.Link>
         </Navbar.Collapse>
       </Navbar>
